8349aea4d01c69503e912f404c86c0a59099ff34
RaiderTs <63519514+RaiderTs@users.noreply.github.com>
2020-12-26 11:05:36 +0200

    исправляет ошибки

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,21 +2,37 @@ import PropTypes from 'prop-types';
 import style from './ContactList.module.css';
 
 function ContactList({ contacts, onRemove }) {
-  if (contacts.length === 0) return null;
   return (
-    <ul className={style.list}>
-      {contacts.map(({ id, name, phone }) => (
-        <li className={style.item} key={id}>
-          <p className={style.info}>
-            {name}: {phone}
-          </p>
-          <button className={style.btn} onClick={() => onRemove(id)}>
-            Remove
-          </button>
-        </li>
-      ))}
-    </ul>
+    contacts.length !== 0 && (
+      <ul className={style.list}>
+        {contacts.map(({ id, name, phone }) => (
+          <li className={style.item} key={id}>
+            <p className={style.info}>
+              {name}: {phone}
+            </p>
+            <button className={style.btn} onClick={() => onRemove(id)}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    )
   );
+  // if (contacts.length === 0) return null;
+  // return (
+  //   <ul className={style.list}>
+  //     {contacts.map(({ id, name, phone }) => (
+  //       <li className={style.item} key={id}>
+  //         <p className={style.info}>
+  //           {name}: {phone}
+  //         </p>
+  //         <button className={style.btn} onClick={() => onRemove(id)}>
+  //           Remove
+  //         </button>
+  //       </li>
+  //     ))}
+  //   </ul>
+  // );
 }
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
